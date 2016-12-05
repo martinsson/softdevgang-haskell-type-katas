a37f9a0fd86ae6cf85c97ac88bd2d386d8d4c4f2
Johan MARTINSSON <martinsson.johan@gmail.com>
2016-12-05 13:31:03 +0100

    got the type constructor Hand to compile

--- src/Game.hs
+++ src/Game.hs
@@ -1,9 +1,23 @@
 module Game where
 
-data Sign = Rock | Scissors
-data Result = Player1Won | Player2Won deriving (Eq, Show)
+data Sign = Rock | Scissors | Paper deriving (Show, Eq)
+data Result = Player1Won | Player2Won | Draw deriving (Eq, Show)
 
 play :: Sign -> Sign -> Result
 play Rock Scissors = Player1Won
 play Scissors Rock = Player2Won
+-- skip further combinations, nothing new here
 
+
+-- thoughts on correctness using types:
+-- 1st level of correctness: input/output only of certain types
+-- 2nd level of correctness: behaviour/invariants in type system
+
+data Player = Player1 | Player2 deriving (Show, Eq)
+
+data Hand = Hand {p :: Player, s :: Sign} deriving (Show, Eq)
+
+data Winner = Draaw | Winner Player deriving (Show, Eq)
+
+winnerOf :: Hand -> Hand -> Winner
+winnerOf _ _ = Draaw
--- src/Game_spec.hs
+++ src/Game_spec.hs
@@ -4,15 +4,9 @@ import Game
 main = hspec $ do
     describe "toto" $ do
         it "tata" $ do
+
             play Rock Scissors `shouldBe` Player1Won
             play Scissors Rock `shouldBe` Player2Won
---            play Player1Rock Player2Rock `shouldBe` Draw
---            play Player2Rock Player1Rock `shouldBe` Draw
---            play Player2Rock Player2Rock `shouldBe` Draw
 
-
-
---inputs
---- tada
---
---g
+            winnerOf ((Hand Player1) Rock) ((Hand Player2) Rock) `shouldBe` Draaw
+--            winnerOf Hand(Player1, Rock) Hand(Player2, Paper) `shouldBe` Winner(Player2)
